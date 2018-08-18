ba5a4eeef8a5ca886ac4530958017512fff4e645
Martin Schoen <schoenm@cip.ifi.lmu.de>
2018-08-18 16:20:31 +0200

    export LearningSearchTermExtractor

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const ArticlePreprocessor = require('./modules/preprocessor/pp-article')
 const KeywordsPreprocessor = require('./modules/preprocessor/pp-keywords')
 const Matcher = require('./modules/training/matcher')
 const StatisticalSearchTermExtractor = require('./modules/search-term-extraction/ste-statistical')
+const LearningSearchTermExtractor = require('./modules/search-term-extraction/ste-learning')
 
 module.exports = {
     ArticlePreprocessor,
     KeywordsPreprocessor,
     Matcher,
-    StatisticalSearchTermExtractor
+    StatisticalSearchTermExtractor,
+    LearningSearchTermExtractor
 }
